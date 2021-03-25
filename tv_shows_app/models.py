386b1c5f5a55b3db1e9d0d1e8cb1535dcedb3b91
MyMac <mymac@Joels-MacBook-Pro.local>
2021-03-25 02:22:45 -0500

    first commit

--- tv_shows_app/models.py
+++ tv_shows_app/models.py
@@ -5,12 +5,20 @@ class ShowsManager(models.Manager):
         errors = {}
         if len(shows_data['title']) < 2:
             errors['title'] = "Title should be at least 2 characters"
+        if len(shows_data['title']) == 0:
+            errors['title'] = "Enter a title"
         if len(shows_data['network']) < 2:
             errors['network'] = "Network should be at least 2 characters"
+        if len(shows_data['network']) == 0:
+            errors['network'] = "Enter a network"
         if len(shows_data['release_date']) < 5:
             errors['release_date'] = "Add a date"
+        if len(shows_data['release_date']) == 0:
+            errors['release_date'] = "Enter a date"
         if len(shows_data['desc']) < 10:
             errors['desc'] = "Description should be at least 10 characters"
+        if len(shows_data['desc']) == 0:
+            errors['desc'] = ""
         return errors
 
 class Show(models.Model):
